46fbfe1250b7145abc94e44fbb3500dd3142385c
Arash <1991a1369@gmail.com>
2024-11-23 15:56:26 +0330

    Test For Check csproj File

--- AMPMI/AQS_Persistence/Contexts/DataBaseContext.cs
+++ AMPMI/AQS_Persistence/Contexts/DataBaseContext.cs
@@ -1,5 +1,6 @@
 ﻿using AQS_Aplication.Interfaces.Context;
 using AQS_Domin.Entities;
+using AQS_Persistence.ShadowPropertiesSetting;
 using Microsoft.EntityFrameworkCore;
 namespace AQS_Persistence.Contexts
 {
@@ -7,10 +8,17 @@ namespace AQS_Persistence.Contexts
     {
         public DataBaseContext(DbContextOptions options) : base(options)
         {
-
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Notification> Notifications { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            ShadowPropertiesHelper.Test();
+
+        }
+
     }
 }
---
+++ AMPMI/AQS_Persistence/ShadowPropertiesSetting/ShadowPropertiesHelper.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AQS_Persistence.ShadowPropertiesSetting
+{
+    internal static class ShadowPropertiesHelper
+    {
+        public static void Test()
+        { }
+    }
+}
