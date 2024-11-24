6a286dd70f2a308642a0f7aa1c6184ea1e3c3f73
Farshid Zeynalzadeh <f.zeinalzadeh83@gmail.com>
2024-11-24 22:50:35 +0330

    init login Controller and View

---
+++ AMPMI/WebSite.EndPoint/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.EndPoint.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string mobmail)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return NotFound();
+        }
+    }
+}
