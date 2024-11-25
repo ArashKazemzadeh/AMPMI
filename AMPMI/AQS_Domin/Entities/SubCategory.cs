a006c8595a1f70bc3e06f8b0a8d295cbdc4adf51
Arash Kazemzadeh <kazemzade.arash13@gmail.com>
2024-11-25 22:46:26 +0330

    Develop : Entities - Config DbContext

---
+++ AMPMI/AQS_Domin/Entities/SubCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace Domin.Entities;
+public partial class SubCategory
+{
+    public int Id { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual Category? Category { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
