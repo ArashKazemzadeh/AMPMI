afed12e97b225d7a7be516c5c8a884dbcc94bbe3
Arash Kazemzadeh <kazemzade.arash13@gmail.com>
2024-12-01 17:25:18 +0330

    Create Areas For Panel

--- AMPMI/WebSite.EndPoint/Startup.cs
+++ AMPMI/WebSite.EndPoint/Startup.cs
@@ -12,7 +12,6 @@ namespace WebSite.EndPoint
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -25,7 +24,6 @@ namespace WebSite.EndPoint
             #endregion
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -35,7 +33,7 @@ namespace WebSite.EndPoint
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+               
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -50,6 +48,10 @@ namespace WebSite.EndPoint
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
