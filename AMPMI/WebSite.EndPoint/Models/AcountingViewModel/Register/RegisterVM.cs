0895a658aad4d7efb8ad80d11d4b5f8e718317aa
Farshid <farshid.zeinazladeh@tookatech.com>
2024-12-04 14:07:53 +0330

    Add View Model And Validation For Register

---
+++ AMPMI/WebSite.EndPoint/Models/AcountingViewModel/Register/RegisterVM.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebSite.EndPoint.Models.AcountingViewModel.Register
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "شماره تلفن را وارد نمایید")]
+        [Display(Name = "شماره تلفن")]
+        [DataType(DataType.PhoneNumber)]
+        public string Mobile { get; set; }
+        [Required(ErrorMessage = "نام شرکت را وارد نمایید")]
+        [Display(Name = "نام شرکت")]
+        public string CompanyName { get; set; }
+        [Required(ErrorMessage = "نام مدیر را وارد نمایید")]
+        [Display(Name = "نام مدیر")]
+        public string ManagerName { get; set; }
+        [Required(ErrorMessage = "ایمیل را وارد نمایید")]
+        [Display(Name = "ایمیل")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "آدرس را وارد نمایید")]
+        [Display(Name = "آدرس")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "پسورد را وارد نمایید")]
+        [Display(Name = "پسورد")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "تکرار پسورد را وارد نمایید")]
+        [Display(Name = "تکرار پسورد")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "تکرار پسورد باید با پسورد یکی باشد")]
+        public string PasswordConfirm { get; set; }
+    }
+}
