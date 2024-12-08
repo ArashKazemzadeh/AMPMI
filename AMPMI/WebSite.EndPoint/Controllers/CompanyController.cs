d6076c2a6f8f9a0daf6d4200cb541ae90990182e
Farshid Zeynalzadeh <f.zeinalzadeh83@gmail.com>
2024-12-09 00:07:04 +0330

    Enhance Company Controlelr With CompanyList View

---
+++ AMPMI/WebSite.EndPoint/Controllers/CompanyController.cs
@@ -0,0 +1,50 @@
+﻿using AQS_Aplication.Interfaces.IServisces;
+using Domin.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+using WebSite.EndPoint.Models.CompanyViewModel;
+
+namespace WebSite.EndPoint.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly ICompanyService _companyService;
+        public CompanyController(ICompanyService companyService )
+        {
+            _companyService = companyService;
+        }
+        public async Task<IActionResult> CompanyList()
+        {
+            List<Company> result = await _companyService.Read();
+            List<CompanyDetailVM> resultVM = result.Select(x => new CompanyDetailVM 
+            {
+                Address = x.Address,
+                LogoRout = x.LogoRout,
+                MobileNumber = x.MobileNumber,
+                Name = x.Name,
+                TeaserGuid = x.TeaserGuid
+            }).ToList();
+
+            return View(resultVM);
+        }
+        public async Task<IActionResult> CompanyDetail(int companyId)
+        {
+            Company? result = await _companyService.ReadById(companyId);
+            if (result != null)
+            {
+                CompanyDetailVM companyDetailVM = new CompanyDetailVM()
+                {
+                    LogoRout = result.LogoRout,
+                    Address = result.Address,
+                    MobileNumber = result.MobileNumber,
+                    Name = result.Name,
+                    TeaserGuid = result.TeaserGuid
+                };
+                
+                return View(companyDetailVM);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ AMPMI/WebSite.EndPoint/Models/CompanyViewModel/CompanyDetailVM.cs
@@ -0,0 +1,14 @@
+﻿namespace WebSite.EndPoint.Models.CompanyViewModel
+{
+    public class CompanyDetailVM
+    {
+        public string Name { get; set; } = null!;
+        public string MobileNumber { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public Guid? LogoRout { get; set; }
+        public string? TeaserGuid { get; set; }
+
+        // TODO : Where is main photo for company
+
+    }
+}
