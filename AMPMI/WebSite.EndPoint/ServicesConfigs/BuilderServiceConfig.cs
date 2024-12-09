e4af20ba66d789623190ce65366e70279db3e4aa
Arash <1991a1369@gmail.com>
2024-12-09 18:18:54 +0330

    edit program.cs

---
+++ AMPMI/WebSite.EndPoint/ServicesConfigs/BuilderServiceConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace WebSite.EndPoint.ServicesConfigs
+{
+    public static class BuilderServiceConfig
+    {
+        public static void AddServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddAuthentication();
+            builder.Services.AddAuthorization();
+            builder.Services.AddMemoryCache();
+        }
+    }
+}
---
+++ AMPMI/WebSite.EndPoint/ServicesConfigs/DependencyInjectionConfig.cs
@@ -0,0 +1,19 @@
+﻿using AQS_Aplication.Interfaces.IInfrastructure.IAuthenticationOTPService;
+using AQS_Aplication.Interfaces.IServisces.IdentityServices;
+using AQS_Aplication.Interfaces.IServisces.IThirdParitesServices;
+using AQS_Aplication.Services;
+using ThirdParties.SMSService;
+using YourNamespace.Services;
+
+namespace WebSite.EndPoint.ServicesConfigs
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void AddScopeds(WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IRegistrationService, RegistrationService>();
+            builder.Services.AddScoped<IAuthenticationOTP, AuthenticationOTP>();
+            builder.Services.AddScoped<ISMSOTPService, SMSOTPService>();
+        }
+    }
+}
