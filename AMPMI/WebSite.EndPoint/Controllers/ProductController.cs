7df2e3acd96c137a07d3ff5a3ceb10662a9469b2
Farshid <farshid.zeinazladeh@tookatech.com>
2024-12-15 16:51:34 +0330

    Develop ProductDetail Action

---
+++ AMPMI/WebSite.EndPoint/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using AQS_Aplication.Interfaces.IServisces;
+using Domin.Entities;
+using Microsoft.AspNetCore.Mvc;
+using WebSite.EndPoint.Models.ProductViewModel;
+
+namespace WebSite.EndPoint.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;  
+        public ProductController(IProductService productService,ICompanyService companyService)
+        {
+            this._productService = productService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ProductDetail(int productId)
+        {
+            Product product = await _productService.ReadById(productId);
+            if(product != null)
+            {
+                ProductDetailVM productDetailVM = new ProductDetailVM() 
+                {
+                    Name = product.Name,
+                    Description = product.Description,
+                    PictureFileName = product.PictureFileName,
+                    SubCategoryId = product.SubCategoryId,
+                    CompanyId = product.CompanyId ?? -1,
+                    CompanyName = product.Company?.Name,
+                    CompanyLogoRout = product.Company?.LogoRout
+                };
+
+                return View(productDetailVM);
+            }
+            //else
+            //{
+            //    //just for test
+            //    ProductDetailVM productDetailVM = new ProductDetailVM()
+            //    {
+            //        Name = "محصول تست",
+            //        Description = "چرتو پرت",
+            //        PictureFileName = null,
+            //        SubCategoryId = 2,
+            //        CompanyId = 1,
+            //        CompanyName = "تست است",
+            //        CompanyLogoRout = null
+            //    };
+            //    return View(productDetailVM);
+            //}
+            return NotFound();
+        }
+    }
+}
