2dad505eaeca63981e78d0bc92faa0555be766d6
atiye.shah@gmail.com <atiye.shah@gmail.com>
2024-12-23 17:32:39 +0330

    Atiye-Css-UserList-EditUser-20241223

---
+++ AMPMI/WebSite.EndPoint/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.EndPoint.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        public IActionResult UserList()
+        {
+            return View();
+        }
+        public IActionResult EditUser()
+        {
+            return View();
+        }
+    }
+}
