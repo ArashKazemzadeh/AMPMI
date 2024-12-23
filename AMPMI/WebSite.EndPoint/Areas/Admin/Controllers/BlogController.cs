537a3192ac963f898d70c00006674ff2bac1be86
Farshid <farshid.zeinazladeh@tookatech.com>
2024-12-23 16:25:05 +0330

    Add BlogList And EditBlog View

---
+++ AMPMI/WebSite.EndPoint/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.EndPoint.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        public IActionResult BlogList()
+        {
+            return View();
+        }
+        public IActionResult EditBlog()
+        {
+            return View();
+        }
+    }
+}
