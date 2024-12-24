8c84571f63f3038fbf7dd14c9aa89d703f0abe14
atiye.shah@gmail.com <atiye.shah@gmail.com>
2024-12-24 17:08:28 +0330

    Atiye-AdminPages-CompanyProfile-CompanyPic

---
+++ AMPMI/WebSite.EndPoint/Areas/Company/Controllers/CompanyPictureController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.EndPoint.Areas.Company.Controllers
+{
+    [Area("Company")]
+    public class CompanyPictureController : Controller
+    {
+        public IActionResult EditCompanyPicture()
+        {
+            return View();
+        }
+    }
+}
