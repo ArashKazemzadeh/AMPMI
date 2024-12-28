7c7185bbfb59fda1f2570f5b45f582e660b8f4d2
Arash Kazemzadeh <wwww.kazemzade.arash13@gmail.com>
2024-12-28 21:47:09 +0330

    edit code

--- AMPMI/AQS_Aplication/Interfaces/IServisces/IdentityServices/IRegistrationService.cs
+++ AMPMI/AQS_Aplication/Interfaces/IServisces/IdentityServices/IRegistrationService.cs
@@ -5,7 +5,7 @@ namespace AQS_Application.Interfaces.IServices.IdentityServices
     public interface IRegistrationService
     {
         Task<ResultRegisterIdentityDto> RegisterAsync(RegisterIdentityDTO registerIdentityDTO, string role);
-        Task<ResultRegisterIdentityDto> ChangePasswordAsync(long userId, string currentPassport, string newPassword)
+        Task<ResultRegisterIdentityDto> ChangePasswordAsync(long userId, string currentPassport, string newPassword);
     }
 
 }
