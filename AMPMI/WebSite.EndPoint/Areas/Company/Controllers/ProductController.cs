cdcd76bcf585ef6fe0605f2cf01fff50343239f7
Farshid Zeynalzadeh <f.zeinalzadeh83@gmail.com>
2024-12-28 20:50:16 +0330

    Develop ProductList And Edit Product Actions

--- AMPMI/WebSite.EndPoint/Areas/Company/Controllers/ProductController.cs
+++ AMPMI/WebSite.EndPoint/Areas/Company/Controllers/ProductController.cs
@@ -1,17 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AQS_Aplication.Interfaces.IServisces.BaseServices;
+using Domin.Entities;
+using Microsoft.AspNetCore.Mvc;
+using WebSite.EndPoint.Areas.Company.Models.Product;
 
 namespace WebSite.EndPoint.Areas.Company.Controllers
 {
     [Area("Company")]
     public class ProductController : Controller
     {
-        public IActionResult ProductList()
+        private readonly IProductService _productService;
+        private readonly ISubCategoryService _subCategoryService;
+        public ProductController(IProductService productService, ISubCategoryService subCategoryService)
         {
-            return View();
+            this._productService = productService;
+            this._subCategoryService = subCategoryService;
         }
-        public IActionResult EditProduct()
+        public async Task<IActionResult> ProductList()
         {
-            return View();
+            long companyId = 9;
+            List<Product> data = await _productService.ReadByCompanyId(companyId);
+            int rowNum = 1;
+            List<ListProductVM> products = data.Select(x => new ListProductVM()
+            {
+                RowNum=rowNum++,
+                Id = x.Id,
+                CompanyId = x.CompanyId,
+                Description = x.Description,
+                Name = x.Name,
+                IsConfirmed = x.IsConfirmed,
+                PictureFileName = x.PictureFileName,
+                SubCategoryName = x.SubCategory.Name,
+                CategoryName = x.SubCategory.Category.Name
+            }).ToList();
+
+            return View(products);
+        }
+        public async Task<IActionResult> EditProduct(long id)
+        {
+            Product product = await _productService.ReadById(id);
+            if (product != null)
+            {
+                List<SubCategory> subCategories = await _subCategoryService.ReadAll();
+                return View(new ProductVM() 
+                {
+                    Id = product.Id,
+                    CompanyId = product.CompanyId,
+                    Description = product.Description,
+                    Name = product.Name,
+                    IsConfirmed = product.IsConfirmed,
+                    PictureFileName = product.PictureFileName,
+                    SubCategoryId = product.SubCategoryId,
+                    CategoryId = product.SubCategory.CategoryId,
+                    SubCategories = subCategories
+                });
+            }
+            return NotFound();
+        }
+        public async Task<IActionResult> DeleteProduct(long id)
+        {
+            Product product = await _productService.ReadById(id);
+            if (product != null)
+            {
+                await _productService.Delete(id);
+                return RedirectToAction(nameof(ProductList));
+            }
+            return NotFound();
         }
         //[HttpPost]
         //public IActionResult NewProduct()
---
+++ AMPMI/WebSite.EndPoint/Areas/Company/Models/Product/ProductVM.cs
@@ -0,0 +1,23 @@
+﻿using Domin.Entities;
+
+namespace WebSite.EndPoint.Areas.Company.Models.Product
+{
+    public class ProductVM
+    {
+        public long Id { get; set; }
+        public int RowNum { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public Guid? PictureFileName { get; set; }
+
+        public bool IsConfirmed { get; set; }
+
+        public long? CompanyId { get; set; }
+        public int SubCategoryId { get; set; }
+        public int CategoryId { get; set; }
+        public List<SubCategory> SubCategories { get; set; }
+    }
+}
