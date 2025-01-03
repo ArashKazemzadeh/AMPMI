9b5c5a3702013e8e31a54e82ba05e3fc31fa3473
Farshid Zeynalzadeh <f.zeinalzadeh83@gmail.com>
2025-01-03 13:08:41 +0330

    Develop Company Teaser Actions

--- AMPMI/WebSite.EndPoint/Areas/Company/Controllers/CompanyProfileController.cs
+++ AMPMI/WebSite.EndPoint/Areas/Company/Controllers/CompanyProfileController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AQS_Application.Interfaces.IServices.BaseServices;
+using AQS_Common.Enums;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebSite.EndPoint.Areas.Company.Controllers
 {
     [Area("Company")]
     public class CompanyProfileController : Controller
     {
+        private readonly ICompanyService _companyService;
+        public CompanyProfileController(ICompanyService companyService)
+        {
+            this._companyService = companyService;        
+        }
         public IActionResult EditCompanyProfile()
         {
             return View();
@@ -13,10 +20,68 @@ namespace WebSite.EndPoint.Areas.Company.Controllers
         {
             return View();
         }
-        //[HttpPost]
-        //public IActionResult ChangePassword(string password)
-        //{
-        //    return View();
-        //}
+        public async Task<IActionResult> EditTeaser(string msg="")
+        {
+            if (!string.IsNullOrEmpty(msg))
+            {
+                ViewData["msg"] = msg;
+            }
+            long companyId = 9;
+            if (User.Identity.IsAuthenticated)
+            {
+                companyId = Convert.ToInt64(User.Identity.Name);
+            }
+            var company = await _companyService.ReadById(companyId);
+            if (company != null)
+            {
+                ViewData["src"] = company.TeaserGuid;
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditTeaser(IFormFile teaser)
+        {
+            if (teaser == null)
+            {
+                return View();
+            }
+            long companyId = 9;
+            if (User.Identity.IsAuthenticated)
+            {
+                companyId = Convert.ToInt64(User.Identity.Name);
+            }
+            string msg = string.Empty;
+            var company = await _companyService.ReadById(companyId);
+            if (company != null)
+            {
+                company.TeaserGuid = "Something"; // TODO : Video Service is unavailable
+                var result = await _companyService.Update(company);
+                if (result == ResultOutPutMethodEnum.savechanged)
+                    msg = "تغییرات با موفقیت ذخیره شد";
+                else
+                    msg = "خطا در هنگام ذخیره اطلاعات ";
+            }
+            return RedirectToAction(nameof(EditTeaser), new { msg = msg });
+        }
+        public async Task<IActionResult> DeleteTeaser()
+        {
+            long companyId = 9;
+            if (User.Identity.IsAuthenticated)
+            {
+                companyId = Convert.ToInt64(User.Identity.Name);
+            }
+            string msg = string.Empty;
+            var company = await _companyService.ReadById(companyId);
+            if (company != null)
+            {
+                company.TeaserGuid = string.Empty;
+                var result = await _companyService.Update(company);
+                if (result == ResultOutPutMethodEnum.savechanged)
+                    msg = "تغییرات با موفقیت ذخیره شد";
+                else
+                    msg = "خطا در هنگام ذخیره اطلاعات ";
+            }
+            return RedirectToAction(nameof(EditTeaser), new { msg = msg });
+        }
     }
 }
