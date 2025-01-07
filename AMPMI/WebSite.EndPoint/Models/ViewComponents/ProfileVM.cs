6155a9d1aa18c2ffb31dca07cfb2ac80ade25aec
Farshid <farshid.zeinazladeh@tookatech.com>
2025-01-07 11:54:45 +0330

    Add userData if user is logged in

---
+++ AMPMI/WebSite.EndPoint/Models/ViewComponents/ProfileVM.cs
@@ -0,0 +1,9 @@
+﻿namespace WebSite.EndPoint.Models.ViewComponents
+{
+    public class ProfileVM
+    {
+        public bool IsLogin { get; set; }
+        public long? UserId { get; set; }
+        public string? UserName { get; set; }
+    }
+}
