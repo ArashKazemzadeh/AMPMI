9de76e878c40333af52d532fce4a3c8ea1e63725
Farshid <farshid.zeinazladeh@tookatech.com>
2025-01-12 12:34:54 +0330

    Develop Blog controller

---
+++ AMPMI/WebSite.EndPoint/Controllers/BlogController.cs
@@ -0,0 +1,36 @@
+﻿using AQS_Application.Dtos.BaseServiceDto.BlogDtos;
+using AQS_Application.Interfaces.IServices.BaseServices;
+using Microsoft.AspNetCore.Mvc;
+using WebSite.EndPoint.Models.Blog;
+
+namespace WebSite.EndPoint.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogService _blogService;
+        public BlogController(IBlogService blogService)
+        {
+            this._blogService = blogService;
+        }
+        public async Task<IActionResult> ShowBlog(int id)
+        {
+            var blog = await _blogService.ReadById(id);
+            if (blog != null)
+            {
+                BlogDetailVM blogDetail = new BlogDetailVM()
+                {
+                    VideoFileName = blog.VideoFileName,
+                    Id = id,
+                    CreateUpdateAt = DateTime.Now,
+                    Description = blog.Description,
+                    HeaderPictureFileName = blog.HeaderPictureFileName,
+                    Subject = blog.Subject,
+                    BlogPictures = blog.BlogPictures.Select(x=>x.Route).ToList()
+                };
+                return View(blogDetail);
+            }
+            return View(new BlogDetailVM());
+        }
+    }
+}
+
