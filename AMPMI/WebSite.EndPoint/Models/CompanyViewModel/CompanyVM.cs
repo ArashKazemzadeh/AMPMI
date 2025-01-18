41666a5c236c21a1a294be5a57d18e5b625b8388
Farshid <farshid.zeinazladeh@tookatech.com>
2025-01-18 17:01:58 +0330

    Debug Company List page - show data

--- AMPMI/WebSite.EndPoint/Models/CompanyViewModel/CompanyVM.cs
+++ AMPMI/WebSite.EndPoint/Models/CompanyViewModel/CompanyVM.cs
@@ -2,11 +2,15 @@
 {
     public class CompanyVM
     {
+        public long Id { get; set; }
         public string Name { get; set; } = null!;
         public string MobileNumber { get; set; } = null!;
+        public string ManagerName { get; set; }
+        public string Email { get; set; }
         public string Address { get; set; } = null!;
         public string? LogoRout { get; set; }
         public string? TeaserGuid { get; set; }
+        public string? Tel { get; set; }
 
         // TODO : Where is main photo for company
 
